59881ffe7b62600f3824f9f980e627d346cbed46
Przemysław Ambroży <prz.ambrozy@gmail.com>
2022-07-02 19:15:27 +0200

    Ada tests to URLCoder

--- Tests/OneTimePassTests/URLCoderTests.swift
+++ Tests/OneTimePassTests/URLCoderTests.swift
@@ -12,26 +12,56 @@ import XCTest
 
 final class URLCoderTests: XCTestCase {
     func testDecode() throws {
-        XCTAssertThrowsError(try HOTP(urlString: "[/]")) { error in
+        XCTAssertThrowsError(try URLCoder("[/]")) { error in
             XCTAssertEqual(error as? OTPError, OTPError.creatingURLComponents)
         }
-        XCTAssertThrowsError(try HOTP(urlString: "http://")) { error in
+        XCTAssertThrowsError(try URLCoder("http://")) { error in
             XCTAssertEqual(error as? OTPError, OTPError.invalidScheme)
         }
-        XCTAssertThrowsError(try HOTP(urlString: "otpauth://a/")) { error in
+        XCTAssertThrowsError(try URLCoder("otpauth://a/")) { error in
             XCTAssertEqual(error as? OTPError, OTPError.invalidType)
         }
-        XCTAssertThrowsError(try HOTP(urlString: "otpauth://hotp/")) { error in
+        XCTAssertThrowsError(try URLCoder("otpauth://hotp/")) { error in
             XCTAssertEqual(error as? OTPError, OTPError.secretMissing)
         }
-        XCTAssertThrowsError(try HOTP(urlString: "otpauth://hotp/?secret")) { error in
+        XCTAssertThrowsError(try URLCoder("otpauth://hotp/?secret")) { error in
             XCTAssertEqual(error as? OTPError, OTPError.secretMissing)
         }
-        XCTAssertEqual(try HOTP(urlString: "otpauth://hotp/?secret=IE&counter=0&counter=1").counter, 1)
-        XCTAssertEqual(try HOTP(urlString: "otpauth://hotp/a?secret=IE&counter=0").account, "a")
 
-        let issuserAccount = try HOTP(urlString: "otpauth://hotp/a:b?secret=IE&counter=0")
+        XCTAssertEqual(try URLCoder("otpauth://hotp/?secret=IE&counter=0&counter=1").counter, 1)
+        XCTAssertEqual(try URLCoder("otpauth://hotp/a?secret=IE&counter=0").account, "a")
+
+        let issuserAccount = try URLCoder("otpauth://hotp/a:b?secret=IE&counter=0")
         XCTAssertEqual(issuserAccount.issuer, "a")
         XCTAssertEqual(issuserAccount.account, "b")
+
+        let urlString =
+            "otpauth://hotp/iss:user?issuer=iss&secret=IE&algorithm=SHA512&digits=8&counter=0&period=30"
+        let urlCoder = try URLCoder(urlString)
+        XCTAssertEqual(urlCoder.type, "hotp")
+        XCTAssertEqual(urlCoder.issuer, "iss")
+        XCTAssertEqual(urlCoder.account, "user")
+        XCTAssertEqual(urlCoder.secret, [0x41])
+        XCTAssertEqual(urlCoder.algorithm, .SHA512)
+        XCTAssertEqual(urlCoder.digits, 8)
+        XCTAssertEqual(urlCoder.counter, 0)
+        XCTAssertEqual(urlCoder.period, 30)
+    }
+
+    func testEncode() throws {
+        let urlCoder = URLCoder(
+            type: "hotp",
+            issuer: "iss",
+            account: "user",
+            secret: [0x41],
+            algorithm: .SHA256,
+            digits: 8,
+            counter: 10,
+            period: 60
+        )
+
+        let expectedString =
+            "otpauth://hotp/iss:user?secret=IE&algorithm=SHA256&digits=8&counter=10&period=60&issuer=iss"
+        XCTAssertEqual(urlCoder.urlString, expectedString)
     }
 }
